8e6423fd60bb7f37b617016913d0b218206724f9
pratik <pratikgaikwad551@gmail.com>
2021-05-14 19:29:44 +0530

    added destructure data

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+
+var arr= [1,"Pratik","SDE"];
+
+var [id,name,role]=arr;
+
+console.log(role);
+
+
+var Student ={
+    user:"1",
+    fname:"Pratik",
+    frole:"SDE",
+}
+
+const {user,fname,frole}=Student;
+console.log(frole);
